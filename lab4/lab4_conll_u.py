752028d970fded6939c7d68cf2ceee908c435be2
Rasmus Olofzon <rasmus.olofzon@gmail.com>
2017-10-10 17:05:17 +0200

    implemented subject-verb and subject-verb-object parts for conll-u format => lab4 done

--- lab4/lab4_conll_u.py
+++ lab4/lab4_conll_u.py
@@ -8,52 +8,44 @@ import conll
 import os
 
 def get_sv_pairs(formatted_corpus):
-
     subj_verb_pairs = {}
     for sentence in formatted_corpus:
-        for word in sentence:
-            if word['deprel'] == 'nsubj':
-                for head in sentence:
-                    if head['id'] == word['head']:
-                        if (word['form'].lower(), head['form'].lower()) in subj_verb_pairs.keys():
-                            subj_verb_pairs[(word['form'].lower(), head['form'].lower())] += 1
+        for word in sentence.items():
+            if word[1]['deprel'] == 'nsubj':
+                for head in sentence.items():
+                    if head[1]['id'] == word[1]['head']:
+                        if (word[1]['form'].lower(), head[1]['form'].lower()) in subj_verb_pairs.keys():
+                            subj_verb_pairs[(word[1]['form'].lower(), head[1]['form'].lower())] += 1
                         else:
-                            subj_verb_pairs[(word['form'].lower(), head['form'].lower())] = 1
+                            subj_verb_pairs[(word[1]['form'].lower(), head[1]['form'].lower())] = 1
     return subj_verb_pairs
-
-
-
 
 def get_sov_triples(formatted_corpus):
 
     sov_triplets = {}
     for sentence in formatted_corpus:
-        for word in sentence:
-            if word['deprel'] == 'nsubj':
-                for other_word in sentence:
-                    #if head['id'] == word['head']: 
-                    if other_word['head'] == word['head'] and other_word['deprel'] == 'obj': 
-                        for head in sentence:
-                            if head['id'] == word['head'] and head['id'] == other_word['head']:
-                                if (word['form'].lower(), other_word['form'].lower(), head['form'].lower()) in sov_triplets.keys():
-                                    sov_triplets[(word['form'].lower(), other_word['form'].lower(), head['form'].lower())] += 1
+        for word in sentence.items():
+            if word[1]['deprel'] == 'nsubj':
+                for other_word in sentence.items():
+                    if other_word[1]['head'] == word[1]['head'] and other_word[1]['deprel'] == 'obj': 
+                        for head in sentence.items():
+                            if head[1]['id'] == word[1]['head'] and head[1]['id'] == other_word[1]['head']:
+                                if (word[1]['form'].lower(), other_word[1]['form'].lower(), head[1]['form'].lower()) in sov_triplets.keys():
+                                    sov_triplets[(word[1]['form'].lower(), other_word[1]['form'].lower(), head[1]['form'].lower())] += 1
                                 else:
-                                    sov_triplets[(word['form'].lower(), other_word['form'].lower(), head['form'].lower())] = 1
+                                    sov_triplets[(word[1]['form'].lower(), other_word[1]['form'].lower(), head[1]['form'].lower())] = 1
     return sov_triplets
-
-
 
 def reformate_corpus(formatted_corpus):
     reformatted_corpus = []
     for sentence in formatted_corpus:
         reformatted_sentence = {}
         for word in sentence:
-            reformatted_sentence[word['id']] = word
+            if '-' not in word['id']:
+                reformatted_sentence[word['id']] = word
         reformatted_corpus.append(reformatted_sentence)
 
     return reformatted_corpus
-                
-
 
 if __name__ == '__main__':
     column_names_u = ['id', 'form', 'lemma', 'upostag', 'xpostag', 'feats', 'head', 'deprel', 'deps', 'misc']
@@ -61,27 +53,28 @@ if __name__ == '__main__':
     print(language)
     train_file = 'datasets/' + language + '-ud-train.conllu'
 
-
     formatted_corpus = conll.split_rows(conll.read_sentences(train_file), column_names_u)
 
-
     reformatted_corpus = reformate_corpus(formatted_corpus)
-    print(formatted_corpus)
 
+    '''
+    for sentence in reformatted_corpus:
+        for word in sentence.items(): 
+            if word[1]['form'] == '%':
+                print(word[1]['form'], word[1]['deprel']) #this printout shows that there in spanish are a lot of %-signs that have dependency relation nsubj to heads, strangely enough
+    '''
 
-    subj_verb_pairs = get_sv_pairs(formatted_corpus)
+    subj_verb_pairs = get_sv_pairs(reformatted_corpus)
     sv_pair_freq = 0
     for freq in subj_verb_pairs.values():
         sv_pair_freq += freq
     print("\nTotal nbr of subject–verb pairs: " + str(sv_pair_freq))
     print("\nThe top 5 most common subject–verb pairs are: ")
-    sorted_pairs = sorted(subj_verb_pairs.items(), key=lambda pair: pair[1])
+    sorted_pairs = sorted(subj_verb_pairs.items(), key=lambda pair: pair[1])  # (subj, verb): 753 
     for i in range(1,6):
         print(sorted_pairs[len(sorted_pairs)-i])
 
-    print('\n')
-
-    sov_triplets = get_sov_triples(formatted_corpus)
+    sov_triplets = get_sov_triples(reformatted_corpus)
     sov_triple_freq = 0
     for freq in sov_triplets.values():
         sov_triple_freq += freq
@@ -90,8 +83,6 @@ if __name__ == '__main__':
     sorted_triplets = sorted(sov_triplets.items(), key=lambda triplet: triplet[1])
     for i in range(1,6):
         print(sorted_triplets[len(sorted_triplets)-i])
-
-
 
 
     '''
