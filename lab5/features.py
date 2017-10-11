bcf0209133967cc507af503e35cdd6f80cc9800b
Rasmus Olofzon <rasmus.olofzon@gmail.com>
2017-10-11 09:40:22 +0200

    added instructive comments for feature.py file

--- lab5/features.py
+++ lab5/features.py
@@ -1,3 +1,15 @@
+'''
+    Returns the features in a dictionary format compatible with scikit-learn. 
+    (You have a code example of feature encoding in this format in the chunking program.)
+'''
 def extract(stack, queue, graph, feature_names, sentence):
     ...
     return features
+
+
+
+#Generate the three scikit-learn models using the code models from the chunking labs. 
+ 
+
+# You will evaluate the model accuracies (not the parsing accuracy) using 
+# the classification report produced by scikit-learn and the correctly classified instances 
